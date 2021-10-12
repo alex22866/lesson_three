2bf28a9c5cd157aa6fc203bf82ae6078812d2fb5
alex22866 <isaeva85sasha@gmail.com>
2021-10-12 16:01:31 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print(ььь):
+    name = "ььь"
+    digit = "228"
+    list_Of_Digits = []
+    print(ььь)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    name = "ььь"
+    digit = "228"
+    list_Of_Digits = [1, 4, 6]
+    str_list = str(list_Of_Digits)
+    print(str_list)
+    print(type(str_list))
+    print(ььь)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
